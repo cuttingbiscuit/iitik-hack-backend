6b1ffcc3141c4ae4d5f89fbbca89f579892e4ce4
iwbas <brexceed@gmail.com>
2020-12-11 23:32:02 +0400

    added comment

--- api/models.py
+++ api/models.py
@@ -38,9 +38,9 @@ class Comment(models.Model):
 
 class StudentGroup(models.Model):
     group = models.ForeignKey(Group, null=False, on_delete=models.CASCADE, related_name='group_fk')
-    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='user_fk')
+    # user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='user_fk')
 
 
 class StudentDiscipline(models.Model):
     discipline = models.ForeignKey(Discipline, null=False, on_delete=models.CASCADE, related_name='discipline_fk')
-    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='user_fk')
+    # user = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='user_fk')
